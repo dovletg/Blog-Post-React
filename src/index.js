e8d2ed6a55d3c5c438751f27b77367ed5ee941d2
jahan <jahan@j-MacBook-Air.home>
2020-12-21 13:32:09 -0500

    Using JSX for adding BlogPost

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const blogPost = {
+    title: 'My first blog',
+    body: 'lorem ipsum dolor sit amet..',
+    published: true
+  }
+ 
+
+let isPublishedClassName;
+if(blogPost.published){
+  isPublishedClassName = 'green'
+} else {
+  isPublishedClassName = 'red'
+}
+
+const element = <h1 className={isPublishedClassName}>{blogPost.title}</h1>;
+
+//function publishThePost(e) {
+  //if(e.target.innerText === 'Publish') {
+   // e.target.innerText = 'Unpublish';
+  //  e.target.parentElement.querySelector('.blog-title').style.color = 'green';
+ // } else {
+  //  e.target.innerText = 'Publish'
+ //   e.target.parentElement.querySelector('.blog-title').style.color = 'red';
+ // }
+//}
+
+//const blogPost = myBlogPosts.map((myBlogPost,index) => {
+ // return (
+//    <div key={'post_' + index}>
+ //     <h1 className={(myBlogPost.published ? 'green' : 'red') + ' blog-title'}>//{myBlogPost.title}</h1>
+ //     <p>{myBlogPost.body}</p>
+ //     <button onClick={publishThePost}>{myBlogPost.published ? 'Unpublish' : //'Publish'}</button>
+  //    <img src="" alt="" />
+  //    <hr/>
+  //  </div>
+  //  
+  //);
+//})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
@@ -15,3 +52,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+/* <React.StrictMode>
+    <App />
+  </React.StrictMode> */
